8698607e3b4f80d2069472853cce64ed0028a13e
mzy <1795498057@qq.com>
2024-04-19 16:25:53 +0800

    back up source files

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@ hexo g
 hexo d
 git add .
 git commit -m "back up source files"
-git remote add origin git@github.com:mzy6666666/mzy6666666.github.io.git
+# git remote add origin git@github.com:mzy6666666/mzy6666666.github.io.git
 git push -u origin hexo
-
